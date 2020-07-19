0d1cecc3e94b584aa648b0bd24f15211ce6a9595
igorN <igornaydennii@gmail.com>
2020-07-19 18:41:05 +0300

    add csrf protection

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const csrf = require('csurf');
 
 const indexRouter = require('./routes/index');
 const productsRouter = require('./routes/products');
 const basketRouter = require('./routes/basket');
 const orderRouter = require('./routes/order');
-require('dotenv').config()
-const {connection, query} = require('./libs/mysql_connection')
-const redis = require('redis')
-const session = require('express-session')
+require('dotenv').config();
+const {connection, query} = require('./libs/mysql_connection');
+const redis = require('redis');
+const session = require('express-session');
 
 var app = express();
 
@@ -21,30 +22,36 @@ app.set('view engine', 'jade');
 
 
 let RedisStore = require('connect-redis')(session)
-let redisClient = redis.createClient()
+let redisClient = redis.createClient({host: process.env.REDIS_HOST})
 
 app.use(
     session({
         store: new RedisStore({ client: redisClient }),
         secret: process.env.SESSION_SECRET,
         resave: false,
+        saveUninitialized: true
     })
 )
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   req.db = query
   next()
 })
 
+app.use(cookieParser());
+app.use(csrf({ cookie: true }))
+const csrfProtection = csrf({ cookie: true });
+
 app.use('/', indexRouter);
 app.use('/products', productsRouter);
 app.use('/basket', basketRouter);
 app.use('/order', orderRouter);
-
+app.get('/test',function(req,res){
+    res.sendFile(path.join(__dirname+'/views/test.html'));
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
